cbe1cef7366262a89113c0af19e49fb397b739e7
Chase Stevens <chase.stevens@hgs.hiddengeniusproject.org>
2023-07-25 14:15:51 -0700

    Shader + Started Map
    - Added shader effect
    - Began building the map
    - Player now has a flashlight

--- ELUSIVITY/Assets/Mimic/Scripts/Movement.cs
+++ ELUSIVITY/Assets/Mimic/Scripts/Movement.cs
@@ -3,93 +3,85 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.SceneManagement;
-
 namespace MimicSpace
 {
-
+    public enum EnemyState
+    {
+        Patrolling,
+        ChasingPlayer
+    }
     /// <summary>
     /// This is a very basic movement script, if you want to replace it
     /// Just don't forget to update the Mimic's velocity vector with a Vector3(x, 0, z)
     /// </summary>
     public class Movement : MonoBehaviour
     {
-        [Header("Controls")]
+        [Header("Mimic Settings"), Space(10)]
+        public EnemyState currentState;
         [Tooltip("Body Height from ground")]
         [Range(0.5f, 5f)]
         public float height = 0.8f;
         public float speed = 5f;
-
+        public float velocityLerpCoef = 4f;
+        Vector3 velocity = Vector3.zero;
+        Mimic myMimic;
+        [Space(10), Header("Patrol Settings"), Space(10)]
+        public List<Transform> patrolPoints;
+        [SerializeField] private int patrolIndex; // The current index of the partol point this is following READ ONLY
         public float minPatrolWaitTime;
         public float maxPatrolWaitTime;
-
-        public List<Transform> patrolPoints;
-        Vector3 velocity = Vector3.zero;
-        public float velocityLerpCoef = 4f;
-        Mimic myMimic;
-
-
-         public GameObject playerTarget; // The Player object which the monster chases
+        [Space(10), Header("Chase Settings"), Space(10)]
+        public GameObject playerTarget; // The Player object which the monster chases
         public float detectionRoadius = 10;
         public float killingRadius = 5;
         private NavMeshAgent _agent;
-        public LayerMask detectableLayers; 
-
-        [SerializeField]
-        private int patrol;
-
+        public LayerMask detectableLayers;
         private void Start()
         {
+            currentState = EnemyState.Patrolling;
             myMimic = GetComponent<Mimic>();
             _agent = GetComponent<NavMeshAgent>();
             StartCoroutine(Wandering());
         }
-
         private IEnumerator Wandering()
         {
             Debug.Log("Wandering");
-            while (true)
+            while (!PlayerInDetectionZone() && currentState == EnemyState.Patrolling)
             {
-
-
                 //generate a random index to patrol to
-                int patrolIndex = Random.Range(0, patrolPoints.Count);
-
-                patrol = patrolIndex;
-
+                int patrol = Random.Range(0, patrolPoints.Count);
+                patrolIndex = patrol;
                 //Patrol to the next point in our list:
                 _agent.SetDestination(patrolPoints[patrolIndex].position);
-
-                //Wait for enemy to reach patrol point
-                while (Vector3.Distance(transform.position, patrolPoints[patrolIndex].position) > 1) yield return null;
-
-                //wait x seconds
+                //Wait for the enemy to reach the patrol point
+                while (Vector3.Distance(transform.position, patrolPoints[patrolIndex].position) > 1)
+                    yield return null;
+                //wait for some time
                 yield return new WaitForSeconds(Random.Range(minPatrolWaitTime, maxPatrolWaitTime));
-                //Generate new move speed
-
-                //Clamp audio to move speed
             }
+            // If the player is detected or the state has changed, switch to chasing player
+            currentState = EnemyState.ChasingPlayer;
+            StartCoroutine(ChasePlayer());
         }
-
-
         void Update()
         {
-
-            /* if (Physics.CheckSphere(transform.position, detectionRoadius, 1 << 6)) //Check if the player is in range or not
-             {
+            // Switch states if needed
+            if (PlayerInDetectionZone() && currentState == EnemyState.Patrolling)
+            {
+                currentState = EnemyState.ChasingPlayer;
                 StopCoroutine(Wandering());
                 StartCoroutine(ChasePlayer());
-             } else
-             {
+            }
+            else if (!PlayerInDetectionZone() && currentState == EnemyState.ChasingPlayer)
+            {
+                currentState = EnemyState.Patrolling;
                 StopCoroutine(ChasePlayer());
                 StartCoroutine(Wandering());
-             } */
-
+            }
             //velocity = Vector3.Lerp(velocity, new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized * speed, velocityLerpCoef * Time.deltaTime);
             velocity = Vector3.Lerp(velocity, _agent.velocity.normalized * speed, velocityLerpCoef * Time.deltaTime);
-
             // Assigning velocity to the mimic to assure great leg placement
             myMimic.velocity = velocity;
-
             transform.position = transform.position + velocity * Time.deltaTime;
             RaycastHit hit;
             Vector3 destHeight = transform.position;
@@ -97,34 +89,35 @@ namespace MimicSpace
                 destHeight = new Vector3(transform.position.x, hit.point.y + height, transform.position.z);
             transform.position = Vector3.Lerp(transform.position, destHeight, velocityLerpCoef * Time.deltaTime);
         }
-
         private IEnumerator ChasePlayer()
         {
             Debug.Log("Chasing");
-            while (true)
+            while (PlayerInDetectionZone() && currentState == EnemyState.ChasingPlayer)
             {
                 _agent.SetDestination(playerTarget.transform.position);
-
-                 if (Physics.CheckSphere(transform.position, killingRadius, 1 << 6)) //Check if the player is in killing range or not
-                 {
+                if (Physics.CheckSphere(transform.position, killingRadius, detectableLayers)) //Check if the player is in killing range or not
+                {
                     print("Kill Player");
                     SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-                 }
-
-                 yield return null;
+                }
+                yield return null;
             }
-            
+            // If the player is no longer detected or the state has changed, switch back to patrolling
+            currentState = EnemyState.Patrolling;
+            StartCoroutine(Wandering());
         }
-
+        private bool PlayerInDetectionZone()
+        {
+            // Returns true if the player is within the detection radius
+            return Physics.CheckSphere(transform.position, detectionRoadius, 1 << 6);
+        }
         void OnDrawGizmosSelected()
         {
             // Display the explosion radius when selected
             Gizmos.color = Color.blue;
             Gizmos.DrawWireSphere(transform.position, detectionRoadius);
-
             Gizmos.color = Color.red;
             Gizmos.DrawWireSphere(transform.position, killingRadius);
         }
     }
-
 }
